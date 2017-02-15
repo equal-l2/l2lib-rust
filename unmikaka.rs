7ee1746b7e1d0f6c7e8e446bfc12db5e9f9a0989
equal-l2 <equal-l2@users.noreply.github.com>
2017-02-15 09:17:23 +0900

    Initial commit

---
+++ unmikaka.rs
@@ -0,0 +1,12 @@
+use std::io::{self,Read};
+mod libmikaka;
+
+fn main(){
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+
+    for c in buf.chars() {
+        print!("{}",libmikaka::from_mikaka(c.to_string().as_str()));
+    }
+}
+
